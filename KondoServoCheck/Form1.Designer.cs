6872715eedcc23b9675ed23b1522c23d744b515d
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2015-01-03 13:36:03 +0900

    Initial Commit

---
+++ KondoServoCheck/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace KondoServoCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonServoOn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonServoOff = new System.Windows.Forms.Button();
+            this.trackBarAngle = new System.Windows.Forms.TrackBar();
+            this.buttonMotion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarAngle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 1500000;
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.ReadTimeout = 1000;
+            // 
+            // buttonServoOn
+            // 
+            this.buttonServoOn.Location = new System.Drawing.Point(16, 16);
+            this.buttonServoOn.Name = "buttonServoOn";
+            this.buttonServoOn.Size = new System.Drawing.Size(88, 23);
+            this.buttonServoOn.TabIndex = 0;
+            this.buttonServoOn.Text = "SERVO ON";
+            this.buttonServoOn.UseVisualStyleBackColor = true;
+            this.buttonServoOn.Click += new System.EventHandler(this.buttonServoOn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonServoOff
+            // 
+            this.buttonServoOff.Location = new System.Drawing.Point(120, 16);
+            this.buttonServoOff.Name = "buttonServoOff";
+            this.buttonServoOff.Size = new System.Drawing.Size(88, 23);
+            this.buttonServoOff.TabIndex = 1;
+            this.buttonServoOff.Text = "SERVO OFF";
+            this.buttonServoOff.UseVisualStyleBackColor = true;
+            this.buttonServoOff.Click += new System.EventHandler(this.buttonServoOff_Click);
+            // 
+            // trackBarAngle
+            // 
+            this.trackBarAngle.Location = new System.Drawing.Point(16, 144);
+            this.trackBarAngle.Maximum = 18000;
+            this.trackBarAngle.Minimum = -18000;
+            this.trackBarAngle.Name = "trackBarAngle";
+            this.trackBarAngle.Size = new System.Drawing.Size(256, 45);
+            this.trackBarAngle.TabIndex = 2;
+            this.trackBarAngle.TickFrequency = 1000;
+            this.trackBarAngle.Scroll += new System.EventHandler(this.trackBarAngle_Scroll);
+            // 
+            // buttonMotion
+            // 
+            this.buttonMotion.Location = new System.Drawing.Point(16, 56);
+            this.buttonMotion.Name = "buttonMotion";
+            this.buttonMotion.Size = new System.Drawing.Size(88, 23);
+            this.buttonMotion.TabIndex = 3;
+            this.buttonMotion.Text = "MOTION";
+            this.buttonMotion.UseVisualStyleBackColor = true;
+            this.buttonMotion.Click += new System.EventHandler(this.buttonMotion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonMotion);
+            this.Controls.Add(this.trackBarAngle);
+            this.Controls.Add(this.buttonServoOff);
+            this.Controls.Add(this.buttonServoOn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarAngle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonServoOn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonServoOff;
+        private System.Windows.Forms.TrackBar trackBarAngle;
+        private System.Windows.Forms.Button buttonMotion;
+    }
+}
+
---
+++ KondoServoCheck/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KondoServoCheck
+{
+    public partial class Form1 : Form
+    {
+        int step = 0;
+        int angle0 = -100;
+        int dir0 = 1;
+        int angle1 = 0;
+        int dir1 = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Cannot Open COM Port");
+                Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            step++;
+            if (step >= 1000) timer1.Stop();
+            angle0 += dir0;
+            if ((angle0 >= 100)||(angle0 <= -100)) dir0 *= -1;
+            angle1 += dir1;
+            if ((angle1 >= 100)||(angle1 <= -100)) dir1 *= -1;
+
+            byte[] com;
+
+            com = B3MLib.B3MLib.Write(
+                0,
+                B3MLib.B3MLib.SERVO_DESIRED_POSITION,
+                new Dictionary<byte, byte[]> {
+                    {0, Extensions.Converter.ByteConverter.Int16ToByteArray(angle0*100)},
+                    {1, Extensions.Converter.ByteConverter.Int16ToByteArray(angle1*100)}});
+            if (B3MLib.B3MLib.Synchronize(serialPort1, com) == false)
+            {
+                MessageBox.Show("Error: Send Command");
+            }
+        }
+
+        private void buttonServoOn_Click(object sender, EventArgs e)
+        {
+            byte[] com;
+
+            com = B3MLib.B3MLib.WriteSingle(
+                0,
+                B3MLib.B3MLib.SERVO_TORQUE_ON,
+                255,
+                new byte[] {
+                    (byte)B3MLib.B3MLib.Options.RunNormal |
+                    (byte)B3MLib.B3MLib.Options.ControlPosition});
+            if (B3MLib.B3MLib.Synchronize(serialPort1, com) == false){
+                MessageBox.Show("Error: Send Command");
+            }
+        }
+
+        private void buttonServoOff_Click(object sender, EventArgs e)
+        {
+            byte[] com;
+
+            com = B3MLib.B3MLib.WriteSingle(
+                0,
+                B3MLib.B3MLib.SERVO_TORQUE_ON,
+                255,
+                new byte[] {
+                    (byte)B3MLib.B3MLib.Options.RunFree |
+                    (byte)B3MLib.B3MLib.Options.ControlPosition});
+            if (B3MLib.B3MLib.Synchronize(serialPort1, com) == false)
+            {
+                MessageBox.Show("Error: Send Command");
+            }
+        }
+
+        private void trackBarAngle_Scroll(object sender, EventArgs e)
+        {
+            byte[] com;
+
+            com = B3MLib.B3MLib.Write(
+                0,
+                B3MLib.B3MLib.SERVO_DESIRED_POSITION,
+                new Dictionary<byte, byte[]> {
+                    {0, Extensions.Converter.ByteConverter.Int16ToByteArray(trackBarAngle.Value)},
+                    {1, Extensions.Converter.ByteConverter.Int16ToByteArray(trackBarAngle.Value)}});
+            if (B3MLib.B3MLib.Synchronize(serialPort1, com) == false)
+            {
+                MessageBox.Show("Error: Send Command");
+            }
+        }
+
+        private void buttonMotion_Click(object sender, EventArgs e)
+        {
+            step = 0;
+            timer1.Start();
+        }
+    }
+}
